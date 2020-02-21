5b03db89f5f703a523b8b3f631442a0cc10c3bf0
ducnguyenlinh <duclinh0305@gmail.com>
2020-02-21 18:24:49 +0900

    Add query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,46 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # User
+    #
+    field :user, Types::UserType, null: false do
+      description 'ユーザ情報を1件取得する'
+      argument :id, Int, required: true, description: 'ユーザID'
+    end
+    def user(id:)
+      User.find(id)
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :users, [Types::UserType], null: false, description: 'ユーザ情報を全件取得する'
+    def users
+      User.all
+    end
+
+    # Post
+    #
+    field :post, Types::PostType, null: false do
+      description 'ポスト情報を1件取得する'
+      argument :id, Int, required: true, description: 'ポストID'
+    end
+    def post(id:)
+      Post.find(id)
+    end
+    field :posts, [Types::PostType], null: false, description: 'ポスト情報を全件取得する'
+    def posts
+      Post.all
+    end
+
+    #Comment
+    #
+    field :comment, Types::CommentType, null: false do
+      description 'コメント情報を1件取得する'
+      argument :id, Int, required: true, description: 'コメントID'
+    end
+    def comment(id:)
+      Comment.find(id)
+    end
+    field :comments, [Types::CommentType], null: false, description: 'コメント情報を全件取得する'
+    def comments
+      Comment.all
     end
   end
 end
