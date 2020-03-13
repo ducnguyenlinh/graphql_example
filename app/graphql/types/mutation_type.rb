4449dc51f1a2e765443773078e0532f44586a7ff
ducnguyenlinh <duclinh0305@gmail.com>
2020-03-13 13:43:22 +0900

    Update app/graphql/types/mutation_type.rb

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_user, mutation: Mutations::CreateUser
+    field :create_post, mutation: Mutations::CreatePost
     field :create_comment, mutation: Mutations::CreateComment
   end
 end
