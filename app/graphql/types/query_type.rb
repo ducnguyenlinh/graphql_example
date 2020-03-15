26dc22883c2c896166863196360384810c676922
ducnguyenlinh <duclinh0305@gmail.com>
2020-03-15 09:51:01 +0900

    Add field :email into app/graphql/types/user_type.rb

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,46 +1,57 @@
 module Types
   class QueryType < Types::BaseObject
+    # ==================================================================================================================
     # User
-    #
-    field :user, Types::UserType, null: false do
-      description 'ユーザ情報を1件取得する'
-      argument :id, Int, required: true, description: 'ユーザID'
-    end
-    def user(id:)
-      User.find(id)
-    end
-
+    # ==================================================================================================================
+    # index
     field :users, [Types::UserType], null: false, description: 'ユーザ情報を全件取得する'
     def users
       User.all
     end
 
+    # show
+    field :user, Types::UserType, null: false do
+      description 'ユーザ情報を1件取得する'
+      argument :id, Int, required: true, description: 'ユーザID'
+    end
+    def user(id:)
+      User.find_by(id: id)
+    end
+
+    # ==================================================================================================================
     # Post
-    #
-    field :post, Types::PostType, null: false do
-      description 'ポスト情報を1件取得する'
-      argument :id, Int, required: true, description: 'ポストID'
-    end
-    def post(id:)
-      Post.find(id)
-    end
+    # ==================================================================================================================
+    # index
     field :posts, [Types::PostType], null: false, description: 'ポスト情報を全件取得する'
     def posts
       Post.all
     end
 
+    # show
+    field :post, Types::PostType, null: false do
+      description 'ポスト情報を1件取得する'
+      argument :id, Int, required: true, description: 'ポストID'
+    end
+    def post(id:)
+      Post.find_by(id: id)
+    end
+
+    # ==================================================================================================================
     #Comment
-    #
+    # ==================================================================================================================
+    # index
+    field :comments, [Types::CommentType], null: false, description: 'コメント情報を全件取得する'
+    def comments
+      Comment.all
+    end
+
+    # show
     field :comment, Types::CommentType, null: false do
       description 'コメント情報を1件取得する'
       argument :id, Int, required: true, description: 'コメントID'
     end
     def comment(id:)
-      Comment.find(id)
-    end
-    field :comments, [Types::CommentType], null: false, description: 'コメント情報を全件取得する'
-    def comments
-      Comment.all
+      Comment.find_by(id: id)
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,7 +1,8 @@
 module Types
   class UserType < Types::BaseObject
     field :id, Int, null: false
-    field :name, String, null: false
+    field :name, String, null: true
+    field :email, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :posts, [Types::PostType], null: false
