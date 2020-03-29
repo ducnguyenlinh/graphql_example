ff6cedb964de601d5be8fd61f205847843518327
ducnguyenlinh <duclinh0305@gmail.com>
2020-03-30 03:06:01 +0900

    Add mutations delete into app/graphql/types/mutation_type.rb

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,9 @@ module Types
     field :update_user, mutation: Mutations::UpdateUser
     field :update_post, mutation: Mutations::UpdatePost
     field :update_comment, mutation: Mutations::UpdateComment
+
+    field :delete_user, mutation: Mutations::DeleteUser
+    field :delete_post, mutation: Mutations::DeletePost
+    field :delete_comment, mutation: Mutations::DeleteComment
   end
 end
